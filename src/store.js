594db4579e9eaf8d934cd578cc73bf22ec7b27ca
Cristin O'Connor <her@cristin.io>
2021-03-20 05:40:06 -0600

    Working on Getters in Lesson 3 https://www.vuemastery.com/courses/mastering-vuex/vuex-state-getters

--- src/store.js
+++ src/store.js
@@ -4,7 +4,36 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Obie Buckets' },
+    // eslint-disable-next-line prettier/prettier
+    categories: ['sustainability','nature','animal welfare','housing','education','food','community'],
+    todos: [
+      { id: 1, text: 'first todo', done: true },
+      { id: 2, text: '2nd todo', done: false },
+      { id: 3, text: 'third todo', done: true },
+      { id: 4, text: '4th todo', done: false }
+    ]
+  },
   mutations: {},
-  actions: {}
+  actions: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length.toString()
+    },
+    // We could have a Getter that gets an array of the todos that are labeled done.
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    // And we can use this Getter inside another Getter if we want to find out how many remaining todos there are to complete.
+    activeTodosCount: (state, getters) => {
+      // Now we are able to return the difference between the number of todos that are done from the total number of todos.
+      return state.todos.length - getters.doneTodos.length
+      // You may be wondering why we wouldn’t just write activeTodos like this instead.
+      // activeTodosCount: (state) => {
+      //   return state.todos.filter(todo => !todo.done).length
+      // }
+      // And we could. This example was just to demonstrate the power of passing in getters to a Getter.
+    }
+  }
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,51 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h4>Create an Event for User ID of&nbsp;<code :style="codeStyles">{{ user.id }}</code></h4>
+    <p>There are currently <b>{{ catLength }}</b> categories. We are displaying the category count with Vuex getters and our component's computed properties.</p>
+    <p>We used a computed property to display {{ user.name }}'s name.</p>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data() {
+    return {
+      codeStyles: {
+        backgroundColor: '#ddd',
+        color: 'red',
+        fontWeight: 400,
+        borderRadius: '4px',
+        lineHeight: '1.1em',
+        padding: '0 2px'
+      }
+    }
+  },
+  // ROUND 4: How to have additional local computed properties
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapState(['user', 'categories'])
+  }
+}
+
+// ROUND 1
+// computed: {
+//    userName() {
+//     return this.$store.state.user.name
+//   },
+//   userId() {
+//     return this.$store.state.user.id
+//   },
+// }
+// ROUND 2
+// computed: mapState({
+//   userName: state => state.user.name,
+//   // categories: state => state.categories
+//   categories: 'categories' // If we’re wanting to access the top-level State (not using dot notation), we can do this
+// })
+// ROUND 3
+// computed: mapState(['categories', 'user']) // If you only need top level state you can even do this
+</script>
